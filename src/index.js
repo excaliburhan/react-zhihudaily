6ec9e993bfee394c4d49118e9a2fa5ebdb0647af
xiaoping <edwardhjp@gmail.com>
2018-04-24 13:53:46 +0800

    update 添加fastclick

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { AppContainer } from 'react-hot-loader'
 import { Provider } from 'react-redux'
+import initReactFastclick from 'react-fastclick'
 import store from '@/redux/store'
 import RouterMap from '@/router'
 import 'xp-reset.css'
 import '@/assets/css/app.css'
+
+initReactFastclick() // 解决移动端click事件
 
 ReactDOM.render(
   <AppContainer>
